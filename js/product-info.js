974fcad590b9fff57a77d64de321bbb03426049f
rominaserrano <rominaserrano94@gmail.com>
2022-09-07 20:11:25 -0300

    entrega 3

---
+++ js/product-info.js
@@ -0,0 +1,26 @@
+//Función que se ejecuta una vez que se haya lanzado el evento de
+//que el documento se encuentra cargado, es decir, se encuentran todos los
+//elementos HTML presentes.
+
+document.addEventListener("DOMContentLoaded", function (e) {
+
+    getJSONData(PRODUCT_INFO_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+
+            category = resultObj.data;
+
+            let categoryNameHTML = document.getElementById("categoryName");
+            let categoryDescriptionHTML = document.getElementById("categoryDescription");
+            let productCountHTML = document.getElementById("productCount");
+            let productCriteriaHTML = document.getElementById("productCriteria");
+
+            categoryNameHTML.innerHTML = category.name;
+            categoryDescriptionHTML.innerHTML = category.description;
+            productCountHTML.innerHTML = category.soldCount;
+            productCriteriaHTML.innerHTML = category.category;
+
+            //Muestro las imagenes en forma de galería
+            showImagesGallery(category.images);
+        }
+    }
+    )})
